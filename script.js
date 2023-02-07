3f62e0b9cdd02b7d4279cd47a1e64af33a7a2611
Ney <nayla.gibson@gmail.com>
2023-02-07 18:32:27 -0400

    ya detecta 2 puntos

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-let x;
-let y;
+let x1;
+let y1;
+
+let x2;
+let y2;
+
+let distance;
+
 function mouse(e){
- let  x=  e.clientX;
- let  y=  e.clientY;
-  console.log(x);
-  console.log(y);
+ x1=  e.clientX;
+ y1=  e.clientY;
+  console.log("x1:" + x1);
+  console.log("y1:" + y1);
+}
+
+function stop(e){
+  x2=  e.clientX;
+  y2=  e.clientY;
+  console.log("x2:" + x2);
+  console.log("y2:" + y2);
+
+  // document.getElementsByClassName("roulette").innerHTML = "hello";
+}
+
+function move(x1, y1, x2, y2) {
+  distance= (x1+x2) ;
+  console.log(distance);
 }
