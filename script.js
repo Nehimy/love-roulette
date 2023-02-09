ccca10e8e51ea7f16ccc1df34dcb59c30e16c85c
Ney <nayla.gibson@gmail.com>
2023-02-09 18:44:09 -0400

    ya gira con el arrastre del mouse

--- script.js
+++ script.js
@@ -6,14 +6,19 @@ let y2;
 
 let distance;
 
-function mouse(e){
+let torque;
+
+document.querySelector('.roulette').ondragstart=rouletteDragStar;
+document.querySelector('.roulette').ondragend=rouletteDragStop;
+
+function rouletteDragStar(e){
  x1=  e.clientX;
  y1=  e.clientY;
   console.log("x1:" + x1);
   console.log("y1:" + y1);
 }
 
-function stop(e){
+function rouletteDragStop(e){
   x2=  e.clientX;
   y2=  e.clientY;
   console.log("x2:" + x2);
@@ -22,7 +27,25 @@ function stop(e){
 }
 
 function move(x1, y1, x2, y2) {
-  distance= Math.sqrt((x2 - x1) + (y2-y1));
+
+  distance= Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2-y1), 2));
+  console.log ("test:" + distance);
   distance= Math.round(distance);
-  console.log(distance);
+  console.log("distance:" + distance);
+
+  torque= Math.round(551/distance);
+  console.log ("torque:" + torque);
+  //document.querySelector('.roulette').style.animation= "rotate " + torque + "s";
+  //document.getElementById("myDIV").style.animation = "mynewmove 4s 2";
+
+  // document.querySelector('.roulette').style.animation= "@keyframes rotate {0%{transform: rotate(0deg);} 100%{transform:rotate(" + torque +"deg);}";
+  // document.querySelector('.roulette').style.animation= "100%{transform: rotate( " + torque + "deg);}";
+
+  // document.querySelector('.roulette').style.animation.keyframes.rotate= "100%{transform: rotate( " + torque + "deg);}";
+
+  //document.querySelector('.roulette').style.animation= "rotate " + torque + "s";
+  //console.log("giros:" + 360 * torque);
+
+  document.querySelector('.roulette').style.transform = "rotate("+Math.pow(360, torque) + "deg)";
+ // document.querySelector('.roulette').style.transform = "rotate("+(360 * torque) + "deg)";
 }
